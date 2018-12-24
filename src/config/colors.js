586d19bb934630670944546b2c97add939f54a10
imgrbs <mixxdarkz2@gmail.com>
2018-12-24 11:21:33 +0700

    add colors and font config

--- src/config/colors.js
+++ src/config/colors.js
@@ -1,3 +1,6 @@
 export default {
-    
+    primary: '#F48572',
+    white: '#fff',
+    text: '#504543',
+    lightText: '#9A8D8A',
 }
---
+++ src/config/fonts.js
@@ -0,0 +1,6 @@
+export default {
+    header: '18px',
+    subheader: '16px',
+    highlight: '14px',
+    content: '12px',
+}
