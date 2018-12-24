67a147395407f12c6e958022a5aaa774e8557de3
imgrbs <mixxdarkz2@gmail.com>
2018-12-24 18:09:59 +0700

    update success

--- src/components/event/success.js
+++ src/components/event/success.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Card } from 'antd'
 
-import Container from '../base/layout';
+import Container, { Col } from '../base/layout';
 import { HeroPrimary, Content } from '../base/text';
 import { ButtonPrimaryOutlined } from '../base/button';
 
@@ -13,14 +13,16 @@ export default class JoinSuccess extends Component {
 
     render() {
         return (
-            <Container className='flex-column py-5'>
-                <HeroPrimary className='mx-auto mt-5'>
-                    สมัครเรียบร้อย !
-                </HeroPrimary>
-                <Card className='m-3'>
-                    <Content className='mb-3 text-center'>เราจะทำการแจ้งเตือนก่อนวันงานเป็นเวลา 1 วัน ผ่านอีเมลของคุณ (ywcvoluteer@gmail.com) หากมีข้อสงสัย ติดต่อเรา 081-111-1111 (คุณแน็ต)</Content>
-                    <ButtonPrimaryOutlined className='w-100 py-2' onClick={this.handleProfile}><b>ไปยังโปรไฟล์ของคุณ</b></ButtonPrimaryOutlined>
-                </Card>
+            <Container className='py-5'>
+                <Col>
+                    <HeroPrimary className='mx-auto mt-5 text-center'>
+                        สมัครเรียบร้อย !
+                    </HeroPrimary>
+                    <Card className='m-3'>
+                        <Content className='mb-3 text-center'>เราจะทำการแจ้งเตือนก่อนวันงานเป็นเวลา 1 วัน ผ่านอีเมลของคุณ (ywcvoluteer@gmail.com) หากมีข้อสงสัย ติดต่อเรา 081-111-1111 (คุณแน็ต)</Content>
+                        <ButtonPrimaryOutlined className='w-100 py-2' onClick={this.handleProfile}><b>ไปยังโปรไฟล์ของคุณ</b></ButtonPrimaryOutlined>
+                    </Card>
+                </Col>
             </Container>
         )
     }
