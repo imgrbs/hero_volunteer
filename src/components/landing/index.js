1d247dca7fe28b36b22fce642c3f1505d0db23f9
imgrbs <mixxdarkz2@gmail.com>
2018-12-24 13:51:07 +0700

    add push rotue

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,29 +1,34 @@
 import React, { Component } from "react"
-import styled from 'styled-components'
 import { Card } from "antd"
 import { isNull } from "util"
 
 import { getAll } from "../../config/firebase"
 
-import Container, { Col, ContainerFluid, VideoContainer } from "../base/layout"
+import Container, { Col } from "../base/layout"
 import Header from "../base/header"
 import { SubHeader, Content } from "../base/text"
 
 export default class LandingIndex extends Component {
   state = {
-    events: [{ title: 'Event name', date: '18 Jan 2019', place: 'The ocean' }, { title: 'Event name2', date: '18 Jan 2019', place: 'The ocean' }],
+    events: [{ title: 'Event name', date: '18 Jan 2019', place: 'The ocean' }, { title: 'Event name2', date: '18 Jan 2019', place: 'The ocean' }, { title: 'Event name', date: '18 Jan 2019', place: 'The ocean' }, { title: 'Event name', date: '18 Jan 2019', place: 'The ocean' }],
     eventsLoading: true
   }
   componentDidMount = () => {
     getAll('/events').once('value').then((snap) => {
       const data = snap.val()
       if (!isNull(data)) {
-        const events = Object.values(snap.val())
-        console.log(events)
+        const keys = Object.keys(snap.val())
+        let events = Object.values(snap.val())
+        events = events.map((event, index) => ({ ...event, uid: keys[index]}))
         this.setState({ events, eventsLoading: false })
       }
     })
   }
+
+  handleClick = eventUid => {
+    this.props.history.push(`/event/${eventUid}`)
+  }
+
   render() {
     const { events, eventsLoading } = this.state
 
@@ -36,12 +41,13 @@ export default class LandingIndex extends Component {
           {
             this.state.events.map((event, i) => {
               return (
-                <Col xs={6} lg={4} className='py-3'>
+                <Col key={i} xs={6} lg={4} className='py-3'>
                   <Card
                     loading={eventsLoading}
                     hoverable
                     style={{ minWidth: 100, maxWidth: 300 }}
                     cover={event.coverImage ? <img src={event.coverImage} /> : null}
+                    onClick={e => this.handleClick(event.uid)}
                   >
                     <SubHeader>{event.eventName}</SubHeader>
                     <Content>{event.date}</Content>
