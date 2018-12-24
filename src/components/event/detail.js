e785747dab8f21a375660311fab38f1c2a9967a6
imgrbs <mixxdarkz2@gmail.com>
2018-12-24 09:39:02 +0700

    add event detail page

---
+++ src/components/event/detail.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import moduleName from 'reactstrap'
+
+export default class EventDetail extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
