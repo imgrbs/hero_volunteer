6f9837defc6f4c2b017ebc3973962204b2506889
imgrbs <mixxdarkz2@gmail.com>
2018-12-24 11:22:58 +0700

    update button style

--- src/components/base/button.js
+++ src/components/base/button.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import styled from 'styled-components'
-import { Button as DefaultButton } from 'reactstrap';
+import { Button as DefaultButton } from 'antd';
+
+import colors from '../../config/colors'
+import fonts from '../../config/fonts'
 
 const Button = ({ children, onClick }) => (
-    <DefaultButton onClick={onClick}>
+    <ButtonPrimary onClick={onClick}>
         { children }
-    </DefaultButton>
+    </ButtonPrimary>
 );
 
 export const ButtonPrimary = styled(DefaultButton)`
+    background-color: ${colors.primary};
+    height: auto;
 `
+
+export const FixedButton = styled(ButtonPrimary)`
+    font-size: ${fonts.header};
+    color: ${colors.white};
+    border: none;
+    position: fixed;
+    border-radius: 15px;
+    width: 100%;
+    font-weight: bold;
+    bottom: 0;
+`
+
 
 export default Button;
