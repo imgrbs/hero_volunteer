c0670735894588cc2137c11df8da6bebcf0c541b
imgrbs <mixxdarkz2@gmail.com>
2018-12-24 11:49:36 +0700

    add facebook button

--- src/components/base/button.js
+++ src/components/base/button.js
@@ -14,17 +14,23 @@ const Button = ({ children, onClick }) => (
 export const ButtonPrimary = styled(DefaultButton)`
     background-color: ${colors.primary};
     height: auto;
+    border-radius: 15px;
+    border: none;
 `
 
 export const FixedButton = styled(ButtonPrimary)`
     font-size: ${fonts.header};
     color: ${colors.white};
-    border: none;
     position: fixed;
-    border-radius: 15px;
     width: 100%;
     font-weight: bold;
     bottom: 0;
+`
+
+export const FacebookButton = styled(ButtonPrimary)`
+    color: ${colors.white};
+    background-color: ${colors.facebook};
+    font-size: ${fonts.highlight};
 `
 
 
