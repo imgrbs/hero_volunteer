507f26549f817c6538b775ecaea3f6167e473bd4
Natchaporn <natchaporn.pon@gmail.com>
2018-12-24 18:09:44 +0700

    Update homepage

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react"
-import { Card } from "antd"
+import { Card, Tag } from "antd"
+import styled from 'styled-components'
+import { Row } from 'reactstrap'
 import { isNull } from "util"
 
 import { getAll } from "../../config/firebase"
@@ -7,10 +9,12 @@ import { getAll } from "../../config/firebase"
 import Container, { Col } from "../base/layout"
 import Header from "../base/text"
 import { SubHeader, Content } from "../base/text"
+import { ButtonPrimaryOutlined } from "../base/button";
+
 
 export default class LandingIndex extends Component {
   state = {
-    events: [{ title: 'Event name', date: '18 Jan 2019', place: 'The ocean' }, { title: 'Event name2', date: '18 Jan 2019', place: 'The ocean' }, { title: 'Event name', date: '18 Jan 2019', place: 'The ocean' }, { title: 'Event name', date: '18 Jan 2019', place: 'The ocean' }],
+    events: [{ title: 'Event name', date: '18 Jan 2019', place: 'The ocean', types: [] }, { title: 'Event name2', date: '18 Jan 2019', place: 'The ocean', types: [] }, { title: 'Event name', date: '18 Jan 2019', place: 'The ocean', types: [] }, { title: 'Event name', date: '18 Jan 2019', place: 'The ocean', types: [] }],
     eventsLoading: true
   }
   componentDidMount = () => {
@@ -19,7 +23,7 @@ export default class LandingIndex extends Component {
       if (!isNull(data)) {
         const keys = Object.keys(snap.val())
         let events = Object.values(snap.val())
-        events = events.map((event, index) => ({ ...event, uid: keys[index]}))
+        events = events.map((event, index) => ({ ...event, uid: keys[index] }))
         this.setState({ events, eventsLoading: false })
       }
     })
@@ -31,32 +35,97 @@ export default class LandingIndex extends Component {
 
   render() {
     const { events, eventsLoading } = this.state
-
+    console.log(this.state.events)
     return (
       <React.Fragment>
+        <div className='w-100' style={{ marginBottom: '20px' }}>
+          <img src='/static/image/banner.png' style={{ width: '100%' }} />
+        </div>
         <Container>
-          <Col>
+
+          <Col className='my-1'>
             <Header>เหมาะกับคุณ</Header>
+            <p>แบ่งปันความรู้สึกดี ๆ จากสิ่งที่คุณชอบ</p>
           </Col>
-          {
-            this.state.events.map((event, i) => {
-              return (
-                <Col key={i} xs={6} lg={4} className='py-3'>
-                  <Card
-                    loading={eventsLoading}
-                    hoverable
-                    style={{ minWidth: 100, maxWidth: 300 }}
-                    cover={event.coverImage ? <img src={event.coverImage} /> : null}
-                    onClick={e => this.handleClick(event.uid)}
-                  >
-                    <SubHeader>{event.eventName}</SubHeader>
-                    <Content>{event.date}</Content>
-                  </Card>
-                </Col>
+          <Col key={0} xs={12} lg={12} className='py-3'>
+            <Card
+              loading={eventsLoading}
+              hoverable
+              style={{ minWidth: 100, maxWidth: 300 }}
+              cover={this.state.events[0].coverImage ? <img src={this.state.events[0].coverImage} /> : null}
+              onClick={e => this.handleClick(this.state.events[0].uid)}
+            >
+              <SubHeader>{this.state.events[0].eventName}</SubHeader>
+              <Content>{this.state.events[0].date}</Content>
+              {typeof this.state.events[0].types === 'string' ? null : this.state.events[0].types.map(type => (<Tag color='#EBE7E6' style={{ color: '#9A8D8A' }} className='py-1'>{type}</Tag>))}
+            </Card>
+          </Col>
+
+          <Row className='px-3 py-5'>
+            <div className='text-center' style={{ verticalAlign: 'middle', width: '280px', height: '50px', backgroundColor: '#F48572' }}>
+              <Header style={{ lineHeight: '50px', color: 'white' }}>รับสมัครทีมหลัก (Core Team)</Header>
+            </div>
+            <Col className='my-1'>
+              <SubHeader style={{ marginTop: '20px' }}>ร่วมกับผู้จัดเพื่อสร้างสรรค์งานให้ผู้ที่ต้องการคุณ</SubHeader>
+            </Col>
+            {
+              this.state.events.map((event, i) => {
+                console.log(event)
+                return (
+                  <Col key={i} xs={6} lg={4} className='py-3'>
+                    <Card
+                      loading={eventsLoading}
+                      hoverable
+                      style={{ minWidth: 100, maxWidth: 300 }}
+                      cover={event.coverImage ? <img src={event.coverImage} /> : null}
+                      onClick={e => this.handleClick(event.uid)}
+                    >
+                      <SubHeader>{event.eventName}</SubHeader>
+                      <Content>{event.date}</Content>
+                      {event.types.length <= 0 ? null : event.types.map(type => (<Tag color='#EBE7E6' style={{ color: '#9A8D8A' }} className='py-1'>{type}</Tag>))}
+                    </Card>
+                  </Col>
+                )
+              }
               )
             }
-            )
-          }
+            <Col className='text-center'>
+              <ButtonPrimaryOutlined style={{ width: '100%', height: '35px' }}>ดูงานทีมหลักเพิ่มเติม</ButtonPrimaryOutlined>
+            </Col>
+          </Row>
+
+          <Row className='px-3 py-5'>
+            <div className='text-center' style={{ verticalAlign: 'middle', width: '280px', height: '50px', backgroundColor: '#F48572' }}><Header style={{ lineHeight: '50px', color: 'white' }}>รับสมัครสตาฟงาน (Staff)</Header></div>
+            <Col className='my-1'>
+              <SubHeader style={{ marginTop: '20px' }}>ร่วมเป็นหนึ่งในร้อย สร้างสิ่งเล็กน้อยให้ยิ่งใหญ่</SubHeader>
+            </Col>
+            {
+              this.state.events.map((event, i) => {
+                console.log(event)
+                return (
+                  <Col key={i} xs={6} lg={4} className='py-3'>
+                    <Card
+                      loading={eventsLoading}
+                      hoverable
+                      style={{ minWidth: 100, maxWidth: 300 }}
+                      cover={event.coverImage ? <img src={event.coverImage} /> : null}
+                      onClick={e => this.handleClick(event.uid)}
+                    >
+                      <SubHeader>{event.eventName}</SubHeader>
+                      <Content>{event.date}</Content>
+                      {event.types.length <= 0 ? null : event.types.map(type => (<Tag color='#EBE7E6' style={{ color: '#9A8D8A' }} className='py-1'>{type}</Tag>))}
+                    </Card>
+                  </Col>
+                )
+              }
+              )
+            }
+            <Col className='text-center'>
+              <ButtonPrimaryOutlined style={{ width: '100%', height: '35px' }}>ดูงานสตาฟเพิ่มเติม</ButtonPrimaryOutlined>
+            </Col>
+          </Row>
+
+
 
         </Container>
       </React.Fragment>
